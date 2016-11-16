e7127c41407f676c03b5ed575c340847e48e52b4
burovgleb <gb@seedr.com>
2016-11-16 16:09:13 +0300

    add lastPhi, lastTheta

--- demos/threeVR/js/DeviceOrientationController.js
+++ demos/threeVR/js/DeviceOrientationController.js
@@ -350,16 +350,16 @@ var DeviceOrientationController = function ( object, domElement ) {
 				// Remove introduced z-axis rotation and add device's current z-axis rotation
 
 				//tmpZ  = rotation.setFromQuaternion( tmpQuat, 'YXZ' ).z;
-				//objZ  = rotation.setFromQuaternion( objQuat, 'YXZ' ).z;
-				//realZ = rotation.setFromQuaternion( deviceQuat || tmpQuat, 'YXZ' ).z;
+				objZ  = rotation.setFromQuaternion( objQuat, 'YXZ' ).z;
+				realZ = rotation.setFromQuaternion( deviceQuat || tmpQuat, 'YXZ' ).z;
 
 				/*rotQuat.set( 0, 0, Math.sin( ( realZ - tmpZ  ) / 2 ), Math.cos( ( realZ - tmpZ ) / 2 ) );
 
 				tmpQuat.multiply( rotQuat );*/
 
-				//rotQuat.set( 0, 0, Math.sin( ( realZ - objZ  ) / 2 ), Math.cos( ( realZ - objZ ) / 2 ) );
+				rotQuat.set( 0, 0, Math.sin( ( realZ - objZ  ) / 2 ), Math.cos( ( realZ - objZ ) / 2 ) );
 
-				//objQuat.multiply( rotQuat );
+				objQuat.multiply( rotQuat );
 
 				this.lastObjQuat.copy( objQuat );
 
